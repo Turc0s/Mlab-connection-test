dc62371eae7091a20da899bc4bd3588e77c5e3ef
tapsa <tapsa872@gmail.com>
2018-02-09 09:36:15 +0200

    First UI made for the todo app

--- mean-todo-mlab/server.js
+++ mean-todo-mlab/server.js
@@ -3,7 +3,6 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var todos = require("./routes/todos");
-// var index = require("./routes/index");
 
 var port = 3000;
 
@@ -15,7 +14,6 @@ app.use(bodyParser.urlencoded( {extended: false } ));
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/api", todos);
-// app.use("/", index);
 
 app.listen(port, () => {
     console.log("Server started on port " + port + " ...");
